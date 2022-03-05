c2eecf0e57b509670aca869cc4ab2909f544858e
Michael <edelsteinmichael23@gmail.com>
2022-03-05 01:29:32 -0500

    commit tree 737c56e48603eff760f7cc828c4a4e23cf12ee33

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Code your solutions in this file
+
+//   for ([initilization]; [condition]; [iteration]) {
+//     [loop body]
+//   }
+
+
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+    }
+
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+
+//assignment: for loop
+
+function writeCards(guestNames, eventType) {
+    const firstMessage = []
+    const secondMessage = []
+    for (let i = 0; i < guestNames.length; i++) {
+        firstMessage.push(`Happy ${eventType}, ${guestNames[i]}!`)
+        secondMessage.push(`Thank you, ${guestNames[i]}, for the wonderful ${eventType} gift!`)
+    }
+    console.log(firstMessage)
+    console.log(secondMessage)
+
+ return firstMessage, secondMessage
+ 
+
+}
+
+writeCards(["Guadalupe","Ollie","Aki"], "surprise") 
+
+
+
+// while ([condition]) {
+//   [loop body]
+
+
+function countDown(x) {
+    let startingNumber = x
+    while (startingNumber > -1 ) {
+      console.log(startingNumber--)
+    }
+    return startingNumber    
+ 
+}
+
+countDown(4)
+console.log(countDown)
